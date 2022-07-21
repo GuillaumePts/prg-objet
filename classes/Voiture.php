ff79449dd38278e7cdaf86c87a3b00ac0a286db4
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-21 11:52:05 +0200

    decouverte de private et getter setter

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -1,12 +1,35 @@
 <?php
 
 class Voiture {
-public string $marque;
-public string $couleur;
+private string $marque;
+private string $couleur;
 public int $nbrRoues =4;
+
+// method pour récuperer la marque de la voiture = getter
+public function getMarque(): string{
+    return $this->marque;
+}
+
+// method pour définir la marque de la voiture = setter
+public function setMarque(string $marque): void{
+    $this->marque = $marque;
+}
+
+public function getColor(): string{
+    return $this->couleur;
+}
+
+// method pour définir la marque de la voiture = setter
+public function setColor(string $color): void{
+    $this->couleur = $color;
+}
 
 public function demarrer(){
     echo "<p>la voiture démarre</p>";
+}
+
+public function afficherCouleur():string{
+    return "<p>la voiture est de couleur " . $this->couleur. " </p>";
 }
 
 }
--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 
 
-
+require __DIR__ . '/vendor/autoload.php';
 require './inc/header.php';
 require_once './classes/Voiture.php';
 
 $voiture1 = new Voiture();
-$voiture1->couleur="rouge";
-$voiture1->marque="BMW";
+
+$voiture1->setColor("Noir");
+echo $voiture1->getColor();
+
+
+$voiture1->setMarque("BMW");
+echo $voiture1->getMarque();
+
+
+
+
+
 
 $voiture2 = new Voiture();
-$voiture2->couleur="vert";
-$voiture2->marque="peugeot";
+
+// $voiture2->marque="peugeot";
+$voiture2->setMarque("peugeot");
+echo $voiture2->getMarque();
+
+$voiture2->setColor("vert");
+echo $voiture2->getColor();
+
 $voiture2->demarrer();
 
-dump($voiture1);
-dump($voiture2);
+// dump($voiture1);
+// dump($voiture2);
 require './inc/footer.php';
